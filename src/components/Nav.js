fb17a4bdd798128ed4e0353e23282e7d56ec16dd
nikita3006 <nikitanegi3011@gmail.com>
2024-02-24 22:44:23 +0530

    add header component details

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,17 +8,46 @@ const Nav = () => {
     <div className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
        <nav className='container mx-auto'>
         <div className='w-full bg-black/20 h-[64px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50'>
-          <Link className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
+          <Link 
+            to='home'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            offset={-200}
+            className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
             <BiHomeAlt/>
           </Link>
-          <Link className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
+          <Link 
+              to='about'
+              activeClass='active'
+              smooth={true}
+              spy={true}
+              className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
             <BiUser/>
           </Link>
-          <Link className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
+          <Link
+            to='services'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
             <BsClipboardData/>
           </Link>
-          <Link className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
+          <Link
+           to='work'
+           activeClass='active'
+           smooth={true}
+           spy={true}
+           className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
             <BsBriefcase/>
+          </Link> 
+          <Link
+           to='contact'
+           activeClass='active'
+           smooth={true}
+           spy={true}
+           className='cursor-pointer w-[70px] h-[60px] flex items-center justify-center'>
+            <BsChatSquare/>
           </Link> 
         </div>
       </nav>
