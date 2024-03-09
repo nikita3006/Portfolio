c9f6cb69f0bbbab05ba777dd8f6272fffd7246fe
nikita3006 <nikitanegi3011@gmail.com>
2024-03-09 17:54:52 +0530

    create a modal as a conformation message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  function sendEmail(e) {
-    e.preventDefault();
-    const formData = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      message: e.target.message.value,
-    };
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
     emailjs.send('service_l34pqyp', 'template_c0cc9k2', formData, 'kX_YyJCQot2-l0EjT')
       .then((response) => {
         console.log('Email sent successfully:', response);
+        setShowConfirmationModal(true);
+        setFormData({ name: '', email: '', message: '' }); 
       })
       .catch((error) => {
         console.error('Error sending email:', error);
       });
-  }
-  
-  return(
+  };
+
+  const closeModal = () => {
+    setShowConfirmationModal(false);
+  };
+
+  return (
     <section className='section' id='contact'>
       <div className='container mx-auto '>
         <div className='flex flex-col lg:flex-row'>
-    
+
           <motion.div
             variants={fadeIn("right", 0.3)}
             initial="hidden"
@@ -36,10 +47,10 @@ const Contact = () => {
             <div>
               <h4 className='text-xl uppercase text-accent font-medium mb-0
               tracking-wide'>Get in Touch</h4>
-              <h2 className='text-[45px] lg:text-[70px] leading-none mb-12'>Let's work <br/> together!</h2>
+              <h2 className='text-[45px] lg:text-[70px] leading-none mb-12'>Let's work <br /> together!</h2>
             </div>
           </motion.div>
-        
+
           <motion.form
             variants={fadeIn("left", 0.3)}
             initial="hidden"
@@ -49,22 +60,26 @@ const Contact = () => {
             pb-10 p-4 items-center w-[280px]'
             onSubmit={sendEmail}
           >
-          
+
             <input
               className='bg-transparent border-b py-3 outline-none w-full
               placeholder:text-white focus:border-accent transition-all'
               type='text'
               placeholder='Your Email Id'
               id='email'
+              value={formData.email}
+              onChange={handleChange}
               required
             />
-             
+
             <input
               className='bg-transparent border-b py-3 outline-none w-full
               placeholder:text-white focus:border-accent transition-all'
               type='text'
               placeholder='Your Name'
               id='name'
+              value={formData.name}
+              onChange={handleChange}
               required
             />
             <textarea
@@ -72,12 +87,24 @@ const Contact = () => {
               placeholder:text-white focus:border-accent transition-all resize-none mb-12'
               placeholder='Your Message'
               id='message'
+              value={formData.message}
+              onChange={handleChange}
               required
             ></textarea>
             <button className='btn btn-lg' type="submit">Send Message</button>
           </motion.form>
         </div>
       </div>
+
+     
+      {showConfirmationModal && (
+        <div className="modal fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg text-accent">
+            <p className="text-center text-xl font-bold mb-4">Your email was successfully sent. We'll get back to you!</p>
+            <button onClick={closeModal} className="bg-accent text-white py-2 px-4 rounded-md">Close</button>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
